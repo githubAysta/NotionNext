74d06b13d65a45001d9f775cacfbc9e20aa8dbc6
掌君大大 <aysta@qq.com>
2024-11-13 23:11:02 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -118,9 +118,9 @@ const CONFIG = {
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_2: '加入我的微信讨论分享',
+  HEO_SOCIAL_CARD_TITLE_3: '点击加六师兄微信',
+  HEO_SOCIAL_CARD_URL: 'http://zx.ntun.cn',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
